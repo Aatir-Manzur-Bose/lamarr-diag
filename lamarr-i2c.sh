3e28172b8d189bf66a160cd5fe73cb0d474e9f0e
Aatir-Manzur-Bose <aatir_manzur@bose.com>
2023-11-02 10:00:59 -0400

    i2c clean and fix

--- lamarr-i2c.sh
+++ lamarr-i2c.sh
@@ -1,33 +1,38 @@
 #!/bin/sh
 #Lamarr I2c Test
-. uitls
+source ./utils
 ti() {
-	BUS=$1
-	DEV=$2
-	REG=$3
-	DEVICENAME=$4
-	EXPECTEDVALUE=$5
+        BUS=$1
+        DEV=$2
+        REG=$3
+        DEVICENAME=$4
+        EXPECTEDVALUE=$5
 
-	VAL=$(i2cget -y $BUS $DEV $REG 2>&1)
-	if test -n "$EXPECTEDVALUE" ; then test $VAL = $EXPECTEDVAL; RC=$? ; fi
-	test $RC = 0 log $DEVICENAME okay || log $DEVICENAME failed
+        VAL=$(i2cget -y $BUS $DEV $REG 2>&1)
+        RC=$?
+        if test -n "$EXPECTEDVALUE"; then test $VAL = $EXPECTEDVAL; RC=$?; fi
+        if test $RC = 0; then
+                log $DEVICENAME okay
+        else
+                log $DEVICENAME failed
+        fi
 }
 
 test_all() {
-	while read BUS DEV REG DEVICENAME EXPECTEDVALUE; do
-		ti $BUS $DEV $REG $DEVICENAME $EXPECTEDVALUE;
-	done
+        while read BUS DEV REG DEVICENAME EXPECTEDVALUE; do
+                ti $BUS $DEV $REG $DEVICENAME $EXPECTEDVALUE;
+        done
 }
 
 test -e i2cdevs || fatal "Missing i2cdevs file"
 iterations=1
 while getopts n: flag
 do
-	case "${flag}" in
-		n) iterations=${OPTARG};;
-	esac
+        case "${flag}" in
+                n) iterations=${OPTARG};;
+        esac
 done
 
 seq 1 $iterations | while read x; do
- 	test_all < i2cdevs
+        test_all < i2cdevs
 done
